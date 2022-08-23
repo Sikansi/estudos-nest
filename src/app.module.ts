c4a1010209a8c440cccefcd915a7ec3e4287baf8
Sikansi <joao.lucas.sikansi@globalfinanceiro.com.br>
2022-08-23 14:16:47 -0300

    Dynamic Module adiciona mas comentado por falta de dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminController } from './admin/admin.controller';
 import { AccountController } from './account/account.controller';
+//import { DatabaseModule } from './database/database.module';
+//import { User } from './users/entities/user.entity';
 
 @Module({
   controllers: [AppController, AdminController, AccountController],
   providers: [AppService],
+  // imports: [DatabaseModule.forRoot([User])],
+  // exports: [DatabaseModule],
 })
 export class AppModule {}
