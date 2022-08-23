0fadb5eb4ba1b8183016a381f3b8c2f3562445f3
Sikansi <joao.lucas.sikansi@globalfinanceiro.com.br>
2022-08-23 14:03:25 -0300

    Transforma o cats module em global

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,13 @@
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
+@Global()
 @Module({
     controllers: [CatsController],
     providers: [CatsService],
+    exports: [CatsService],
 })
-export class CatsModule {}
+export class CatsModule {
+    constructor(private catsService: CatsService) {}
+}
