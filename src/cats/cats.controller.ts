e4df030cfac08a345b6acc89cffcca61bfcb1c9e
Sikansi <joao.lucas.sikansi@globalfinanceiro.com.br>
2022-08-23 09:26:47 -0300

    Novos endpoints para o novo controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,30 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post, Query, Redirect, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+    @Post('new')
+    @HttpCode(204)
+    @Header('Cache-Control', 'none')
+    create(): string {
+        return 'This action adds a new cat';
+    }
+    
     @Get('list-all')
     findAll(@Req() request: Request): string{
         return 'This action returns all cats';
+    }
+
+    @Get('ab*cd')
+    find() {
+        return 'This route uses a wildcard';
+    }
+
+    @Get('docs')
+    @Redirect('https://docs.nestjs.com', 302)
+    getDocs(@Query('version') version) {
+        if(version && version === '5') {
+            return { url: 'https://docs.nestjs.com/v5/'};
+        }
     }
 }
